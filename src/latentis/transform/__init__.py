ed1a9ec23c5a871ebcb930213db27c2694655e11
Kin Wai Cheuk <kinwai.cheuk@sony.com>
2024-07-25 14:45:51 +0900

    made everything running

--- src/latentis/transform/__init__.py
+++ src/latentis/transform/__init__.py
@@ -1,9 +1,14 @@
 from ._abstract import Estimator, Identity, SimpleTransform, Transform, TransformSequence
-
+from .base import Centering, MeanLPNorm, StandardScaling, STDScaling, LPNorm
 __all__ = [
     "Transform",
     "Identity",
     "SimpleTransform",
     "TransformSequence",
     "Estimator",
+    "Centering",
+    "STDScaling",
+    "StandardScaling",
+    "LPNorm",
+    "MeanLPNorm",
 ]
